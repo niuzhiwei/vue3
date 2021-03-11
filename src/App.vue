73379ffd51f5f1b7f0777a01cc31129394496235
niuzhiwei <xujin02@qq.com>
2021-03-11 15:39:05 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user='user'></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'niuniu',
+  id: 1
+}
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: '专栏1',
+    description: '这是专栏这是专栏这是专栏这是专栏这是专栏这是专栏这是专栏',
+    avatar:
+      'https://pic3.zhimg.com/80/v2-283ce89cb6a57ee1c5745f2babf6b911_720w.jpg?source=1940ef5c'
+  },
+  {
+    id: 2,
+    title: '专栏2',
+    description: '这是专栏2',
+    avatar: ''
+  },
+  {
+    id: 3,
+    title: '专栏2',
+    description: '这是专栏2',
+    avatar:
+      'https://pic3.zhimg.com/80/v2-283ce89cb6a57ee1c5745f2babf6b911_720w.jpg?source=1940ef5c'
+  }
+]
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ColumnList, GlobalHeader },
+  setup () {
+    return {
+      list: testData,
+      user: currentUser
+    }
   }
 })
 </script>
